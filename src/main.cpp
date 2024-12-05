2541a8c80cbb06701e9c6759ca97fe2dc18658f4
Peng-Rao <rao.peng@icloud.com>
2024-12-05 20:25:12 +0100

    Refactor rsvd code
    I rearranged the original code and used a more similar implementation to
    eigen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,27 @@
+#include "RandomizedSVD.h"
 #include <iostream>
-#include <Eigen/Dense>
-#include <Eigen/SVD>
-#include <Eigen/QR>
-
-
-using namespace std;
-using namespace Eigen;
-
-// define QR class
-class QR {
-public:
-    //construct function
-    QR(MatrixXd A) : A(A) {};
-    //compute matrixQ function
-    MatrixXd getMatrixQ() { 
-        HouseholderQR<MatrixXd> qr(A);
-        MatrixXd Q = qr.householderQ(); 
-        return Q;
-    }
-    //deconstruct function
-    ~QR() = default;
-private:
-    MatrixXd A;
-};
-
-//define powerSVD class
-class PowerSVD {
-public:
-    //construct function
-    PowerSVD(MatrixXd A) : A(A) { m = A.rows(); n = A.cols();};
-    //compute matrixU function
-    MatrixXd getMatrixU() { 
-        JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
-        return svd.matrixU();
-    }
-    //compute matrixV function
-    MatrixXd getMatrixV() { 
-        JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
-        return svd.matrixV();
-    }
-    //compute matrixSigma function
-    MatrixXd getMatrixSigma() {
-        JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
-        return svd.singularValues().asDiagonal();
-    }
-    //deconstruct function
-    virtual ~PowerSVD() = default;
-protected:
-    MatrixXd A;
-    int m,n;
-};
-
-//rSVD extend from PowerSVD
-class rSVD : public PowerSVD {
-public:
-    //construct function
-    rSVD(MatrixXd A, int k) : PowerSVD(A), k(k){};
-    void rotate() {
-        //create random matrix
-        MatrixXd Omega = MatrixXd::Random(n, k); 
-        MatrixXd Y = A * Omega;
-
-        //QR decomposition
-        QR qr(Y);
-        MatrixXd Q = qr.getMatrixQ();
-
-        //rotate
-        A = Q * A;
-        return;
-    }
-    //deconstruct function
-    ~rSVD() override = default;
-private:
-    //col of random matrix
-    int k;
-};
 
 
 int main() {
-    // Set the size of the matrix A
-    int m = 100, n = 50, k = 10;
+    Eigen::MatrixXd A(3, 3);
+    A << 1, 3, 2,
+         5, 3, 1,
+         3, 4, 5;
 
-    // Initialize matrix A with random values
-    MatrixXd A = MatrixXd::Random(m, n);
-    //use matrixA
-    rSVD rsvd(A,k);
-    rsvd.rotate();
+    Eigen::RandomizedSVD<Eigen::MatrixXd> rsvd;
 
-    cout << "U (Left Singular Vectors):" << endl << rsvd.getMatrixU().norm() << endl << endl;
-    cout << "Sigma (Singular Values):" << endl << rsvd.getMatrixSigma().norm() << endl << endl;
-    cout << "Vt (Right Singular Vectors):" << endl << rsvd.getMatrixV().transpose().norm() << endl << endl;
+    constexpr int rank {2};
+    constexpr int powerIter {3};
+
+    rsvd.compute(A, rank, powerIter);
+
+    const auto& singularValues = rsvd.singularValues();
+    const auto& U = rsvd.matrixU();
+    const auto& V = rsvd.matrixV();
+
+    std::cout << "Singular values: " << singularValues.transpose() << std::endl;
+    std::cout << "U matrix: " << U << std::endl;
+    std::cout << "V matrix: " << V << std::endl;
 
     return 0;
 }
