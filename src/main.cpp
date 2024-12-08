2e1765375759bf6b250d8e78538e5e1592b4befd
Peng-Rao <rao.peng@icloud.com>
2024-12-08 16:40:21 +0100

    Add unit test for rsvd
    I used a 500-dimensional Hilbert space for testing, with a test error of
    1e-5.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include "RandomizedSVD.h"
+
 #include <iostream>
 
 
 int main() {
     Eigen::MatrixXd A(3, 3);
-    A << 1, 3, 2,
-         5, 3, 1,
-         3, 4, 5;
+    A << 1, 3, 2, 5, 3, 1, 3, 4, 5;
 
     Eigen::RandomizedSVD<Eigen::MatrixXd> rsvd;
 
-    constexpr int rank {2};
-    constexpr int powerIter {3};
+    constexpr int rank{2};
+    constexpr int powerIter{3};
 
     rsvd.compute(A, rank, powerIter);
 
-    const auto& singularValues = rsvd.singularValues();
-    const auto& U = rsvd.matrixU();
-    const auto& V = rsvd.matrixV();
+    const auto &singularValues = rsvd.singularValues();
+    const auto &U = rsvd.matrixU();
+    const auto &V = rsvd.matrixV();
 
     std::cout << "Singular values: " << singularValues.transpose() << std::endl;
     std::cout << "U matrix: " << U << std::endl;
@@ -25,3 +24,4 @@ int main() {
 
     return 0;
 }
+
