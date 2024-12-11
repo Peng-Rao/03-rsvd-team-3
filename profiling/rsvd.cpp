395485f0ee6490fe4c8d21f552a56448442c979b
Peng-Rao <rao.peng@icloud.com>
2024-12-12 00:34:16 +0100

    Add a new directory to do profiling

--- profiling/rsvd.cpp
+++ profiling/rsvd.cpp
@@ -1,3 +1,16 @@
-//
-// Created by RAO PENG on 12/12/24.
-//
+#define EIGEN_USE_BLAS
+
+#include "RandomizedSVD.h"
+#include <Eigen/Dense>
+
+
+int main() {
+    constexpr int n{2000};
+    constexpr int rank{10};
+    constexpr int powerIterations{2};
+
+    const Eigen::MatrixXd A = Eigen::MatrixXd::Random(n, n);
+    Eigen::RandomizedSVD<Eigen::MatrixXd> rsvd;
+    rsvd.compute(A, rank, powerIterations);
+    return 0;
+}
