568457c48f0bac84660596438429e18be72096f7
Claudio <reiserk338@gmail.com>
2025-01-30 20:45:56 +0100

    Solve conflict again

--- tests/test_pca.cpp
+++ tests/test_pca.cpp
@@ -1,23 +1,53 @@
-#include <catch2/catch_test_macros.hpp>
-#include "PCA.h"
 
-TEST_CASE("PCA Basic Test", "[pca]") {
-    // Test data
-    Eigen::MatrixXd data(5, 3);
-    data << 2.5, 2.4, 1.5,
-            0.5, 0.7, 0.3,
-            2.2, 2.9, 1.6,
-            1.9, 2.2, 1.1,
-            3.1, 3.0, 1.8;
+//
+// Created by 黄家励 on 2025/1/27.
+//
+#include <iostream>
+#include <ctime>
+#include "../applications/PCA/PCA.h"
+using namespace std;
 
-    SECTION("Dimension Reduction") {
-        Eigen::MatrixXd reduced_data = PCA(data, 2, 2);
-        
-        // Check dimensions
-        REQUIRE(reduced_data.rows() == 5);
-        REQUIRE(reduced_data.cols() == 2);
-        
-        // Basic sanity checks
-        REQUIRE(reduced_data.allFinite());
-    }
-} 
+
+int main(){
+    // data example
+    Eigen::MatrixXd data = Eigen::MatrixXd::Random(100,500);
+    int dimension = 50;
+    int rank = dimension;
+
+    clock_t start, end;
+    double elapse;
+
+
+    Eigen::PCA pca;
+    start = clock();
+    pca.compute(data,dimension);
+    end = clock();
+    elapse = double(end - start) / CLOCKS_PER_SEC * 1000;
+    Eigen::MatrixXd reduced_data = pca.reducedMatrix();
+    // cout << "reduced data:\n" << reduced_data << endl << endl;
+    cout << "Common PCA time elapse on: "<< elapse << " ms" << endl;
+
+
+    Eigen::PCA pca_by_svd;
+    start = clock();
+    pca_by_svd.computeBySVD(data,dimension);
+    end = clock();
+    elapse = double(end - start) / CLOCKS_PER_SEC * 1000;
+    Eigen::MatrixXd reduced_data1 = pca.reducedMatrix();
+    // cout << "reduced data:\n" << reduced_data1 << endl << endl;
+    cout << "PCA by SVD time elapse on: "<< elapse << " ms" << endl;
+
+
+    Eigen::PCA pca_by_rsvd;
+    start = clock();
+    pca_by_rsvd.computeByRSVD(data,dimension, rank);
+    end = clock();
+    elapse = double(end - start) / CLOCKS_PER_SEC * 1000;
+    Eigen::MatrixXd reduced_data2 = pca_by_rsvd.reducedMatrix();
+    // cout << "reduced data:\n" << reduced_data2 << endl;
+    cout << "PCA by rSVD time elapse on: "<< elapse << " ms" << endl;
+
+
+
+    return 0;
+}
