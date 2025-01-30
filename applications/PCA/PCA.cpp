898c7fbd33f2a11ad57203a7e7f0ec0593fd3af8
Claudio <reiserk338@gmail.com>
2025-01-30 13:35:57 +0100

    Create PCA class

--- applications/PCA/PCA.cpp
+++ applications/PCA/PCA.cpp
@@ -2,45 +2,9 @@
 // Created by 黄家励 on 2025/1/27.
 //
 #include <iostream>
-#include <Eigen/Dense>
-#include <vector>
-#include "RandomizedSVD.h"
-
+#include "PCA.h"
 using namespace std;
 
-Eigen::MatrixXd PCA(const Eigen::MatrixXd& data, int num_components, int rank) {
-    // Step 1: data centralisation
-    Eigen::RowVectorXd mean = data.colwise().mean();  // get mean value of each col
-    Eigen::MatrixXd centered = data.rowwise() - mean;
-
-    // Step 2: Calculate the eigen value and eigen vector by rSVD instead of calculating covariance matrix
-
-    // Create an instance of RandomizedSVD
-    Eigen::RandomizedSVD<Eigen::MatrixXd> rSVD;
-
-    // Compute the SVD for the current data matrix
-    rSVD.compute(centered, rank);
-    Eigen::VectorXd eigen_values = rSVD.singularValues();       // 特征值
-    Eigen::MatrixXd eigen_vectors = rSVD.matrixV();     // 特征向量
-
-    // Step 4: ordering by eigen value
-    vector<pair<double, Eigen::VectorXd>> eigen_pairs;
-    for (int i = 0; i < eigen_values.size(); ++i) {
-        eigen_pairs.emplace_back(eigen_values(i), eigen_vectors.col(i));
-    }
-    sort(eigen_pairs.rbegin(), eigen_pairs.rend(), [](const auto& a, const auto& b) {
-        return a.first < b.first;
-    });
-
-    // extract num principal components
-    Eigen::MatrixXd projection_matrix(data.cols(), num_components);
-    for (std::vector<std::pair<double, Eigen::VectorXd>>::size_type i = 0; i < num_components; ++i) {
-        projection_matrix.col(i) = eigen_pairs[i].second;
-    }
-
-    // Step 5: data projection
-    return centered * projection_matrix;
-}
 
 int main(){
     // data example
@@ -50,13 +14,13 @@ int main(){
             2.2, 2.9, 1.6,
             1.9, 2.2, 1.1,
             3.1, 3.0, 1.8;
+    cout << "orginal data:\n" << data << endl;
 
-    cout << "原始数据:\n" << data << endl;
-
+    Eigen::PCA pca_by_rsvd;
+    pca_by_rsvd.computeByRSVD(data,2, 3);
     // reduced data
-    Eigen::MatrixXd reduced_data = PCA(data, 2, 2);
-
-    cout << "降维后的数据:\n" << reduced_data << endl;
+    Eigen::MatrixXd reduced_data1 = pca_by_rsvd.reducedMatrix();
+    cout << "reduced data:\n" << reduced_data1 << endl;
 
     return 0;
 }
---
+++ applications/PCA/PCA.h
@@ -0,0 +1,69 @@
+//
+// Created by 黄家励 on 2025/1/30.
+//
+
+#ifndef PCA_H
+#define PCA_H
+
+#endif //PCA_H
+
+#include <iostream>
+#include <vector>
+#include <Eigen/dense>
+#include "RandomizedSVD.h"
+
+using namespace std;
+
+namespace Eigen{
+    class PCA {
+    public:
+        PCA() = default;
+        PCA &compute(const MatrixXd &data, int dimension){
+            dataReduced(data, dimension);
+            return *this;
+        }
+        PCA &computeByRSVD(const MatrixXd &data, int dimension, int rank){
+            dataReducedByRSVD(data, dimension, rank);
+            return *this;
+        }
+        const MatrixXd &reducedMatrix() const {return reducedMat;}
+        ~PCA() = default;
+
+    private:
+        MatrixXd reducedMat;
+        void dataReduced(const MatrixXd &data, int dimension) {
+            // Step 1: data centralisation
+            Eigen::VectorXd mean = data.colwise().mean(); // 每列的均值
+            Eigen::MatrixXd centered = data.rowwise() - mean.transpose(); // 每行减去均值
+
+            // Step 2: Compute the eigen vector by SVD
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd(centered, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+            // Step 3: get principal components
+            Eigen::MatrixXd principal_components = svd.matrixV().leftCols(dimension);
+
+            // Step 4: data projection
+            this->reducedMat = centered * principal_components;
+        }
+
+        void dataReducedByRSVD(const MatrixXd &data, int dimension, int rank) {
+            // Step 1: data centralisation
+            Eigen::VectorXd mean = data.colwise().mean();  // get mean value of each col
+            Eigen::MatrixXd centered = data.rowwise() - mean.transpose();
+
+            // Step 2: Compute the eigen vector by rSVD
+            Eigen::RandomizedSVD<Eigen::MatrixXd> rSVD;
+            rSVD.compute(centered, rank);
+
+            // Step 3: get principal components
+            Eigen::MatrixXd principal_components = rSVD.matrixV().leftCols(dimension);
+
+            // Step 4: data projection
+            this->reducedMat = centered * principal_components;
+        }
+
+
+    };
+
+
+}
